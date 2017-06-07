dffb4fd666da6a1d744729d11d9d49159fe95d00
Alex Bruno Cáceres <git.alexbr@outlook.com>
2017-06-06 23:42:42 -0400

    Total refactoring to production

--- README.md
+++ README.md
@@ -9,6 +9,57 @@ A dead simple, fast and objective CLI package manager for anything without depen
 - Get Gist and Gitlab snippets to save as code
 - All automated by a simple JSON config file
 
-## Docs
+## Install
 
-Coming soon...
+In your project:
+
+```
+npm i -S snipacks
+```
+
+Then, include a new command in `package.json` into `scripts` section:
+
+```json
+{
+	"scripts": {
+		"snipacks": "snipacks"
+	}
+}
+```
+
+Choose a name of your preference, but point it to `snipacks` command.
+
+Or, if you prefer, you can install it globaly:
+
+```
+npm i -g snipacks
+```
+
+Then you can run CLI command at root directory of any project.
+
+## Usage
+
+**Snipacks** reads a section at your `package.json` file that register packages, snippets, gists or anything you want to fetch into your project.
+
+Create a `snipacks` section like this:
+
+```json
+{
+	"snipacks": {
+		"unpkg": {
+            "worktopus.js": "worktopus",
+            "moment.js": "moment/min/moment-with-locales.min.js",
+            "jquery.js": "jquery/dist/jquery.min.js"
+        },
+        "gitlab": {
+            "dom.js": "34295",
+            "dstrbg.styl": "1662645",
+            "tplrender.js": "1663326"
+        },
+		"gist": {
+            "kav.js": "steven89/f7aedca683deee6ee8211399e94cd583",
+            "asq.js": "jyamashiro24/17ac171a73246744b09a47d6c9d77241"
+        }
+	}
+}
+```
--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 #!/usr/bin/env node
+
 'use strict'
 
-const fs = require('fs'),
-	https = require('https'),
-	path = require('path'),
-	proc = require('process'),
-	chroot = proc.cwd(),
-	config = path.join(chroot, 'snipacks.json'),
-	deps = require(config),
-	snipacks = {
-		dir: path.join(chroot, deps.dir),
-		update() {
-			fs.stat(this.dir, (e, s) => e && !s ? fs.mkdir(this.dir, () => this.loop()) : this.loop())
-		},
-		loop() {
-			console.log('Async fetching snippets/packages...')
-			Object.keys(deps).filter(k => k !== 'dir').forEach(k => this[k](deps[k]))
-			console.log(`Your files will be saved at ${this.dir}`)
-		},
-		getfile(url, out) {
-			return https.get(url, resp => {
-				const file = path.join(this.dir, out)
-				return resp.pipe(fs.createWriteStream(file))
-			})
-		},
-		unpkg(deps) {
-			const unpkg = 'https://unpkg.com',
-				npm = 'https://registry.npmjs.org'
-			deps.forEach(dep => {
-				const version = dep.version,
-					url = `${unpkg}/${dep.id}`,
-					getfile = vs => this.getfile(`${url}@${vs}`, dep.file)
+let fs = require('fs'),
+    https = require('https'),
+    path = require('path'),
+    proc = require('process'),
+    cwd = proc.cwd(),
+    cfg = path.join(cwd, 'package.json'),
+    deps = require(cfg).snipacks || {},
+    snipacks = {
+        dir: 'snipacks',
+        update() {
+            fs.stat(this.dir, e => {
+                return e ? fs.mkdir(this.dir, () => this.loop()) : this.loop()
+            })
+        },
+        loop() {
+            console.log('Async fetching snippets/packages...')
+            Object.keys(deps).filter(k => k !== 'dir').forEach(k => this[k](deps[k]))
+            console.log(`Your files will be saved at ${this.dir}`)
+        },
+        getfile(url, sub, out) {
+            const dir = path.join(this.dir, sub),
+                file = path.join(dir, out),
+                request = () => https.get(url, resp => resp.pipe(fs.createWriteStream(file)))
+            fs.stat(dir, e => e ? fs.mkdir(dir, request) : request())
+        },
+        unpkg(packs) {
+            const unpkg = 'https://unpkg.com',
+                npm = 'https://registry.npmjs.org'
 
-				return version ? getfile(version) : https.get(`${npm}/${dep.id}`, resp => {
-					let str = ''
-					resp.on('data', data => str += data)
-					resp.on('end', () => {
-						const json = JSON.parse(str),
-							vrs = json['dist-tags'].latest
-						return getfile(vrs)
-					})
-				})
-			})
-		},
-		gist(deps) {
-			const gist = 'https://gist.githubusercontent.com'
-			deps.forEach(dep => this.getfile(`${gist}/${dep.id}/raw`, dep.file))
-		},
-		gitlabs(deps) {
-			const gitlabs = 'https://gitlab.com/snippets'
-			deps.forEach(dep => this.getfile(`${gitlabs}/${dep.id}/raw`, dep.file))
-		}
-	}
+            Object.keys(packs).forEach(file => {
+                const semver = packs[file].includes('@'),
+                    src = packs[file].split(semver ? '@' : '/'),
+                    pack = `${unpkg}/${src[0]}`,
+                    getfile = req => this.getfile(`${pack}@${req}`, 'unpkg', file)
+
+                return semver ? getfile(src[1]) : https.get(`${npm}/${src[0]}`, resp => {
+                    let str = ''
+                    resp.on('data', data => str += data)
+                    resp.on('end', () => {
+                        const json = JSON.parse(str),
+                            vrs = json['dist-tags'].latest,
+                            rest = (semver ? src[1].split('/') : src).slice(1).join('/'),
+                            req = rest ? `${vrs}/${rest}` : vrs
+                        return getfile(req)
+                    })
+                })
+            })
+        },
+        snippets(type, codes) {
+            const base = {
+                    gist: 'https://gist.githubusercontent.com',
+                    gitlab: 'https://gitlab.com/snippets'
+                },
+                src = base[type]
+            
+            Object.keys(codes).forEach(file => {
+                const id = codes[file]
+                this.getfile(`${src}/${id}/raw`, type, file)
+            })
+        },
+        gist(codes) {
+            this.snippets('gist', codes)
+        },
+        gitlab(codes) {
+            this.snippets('gitlab', codes)
+        }
+    }
 
 snipacks.update.call(snipacks)
